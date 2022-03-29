b2d6473f47f6c8f689925e744cb0c2a3d9c7f486
OliverSu <oliversu15@outlook.com>
2022-03-29 13:49:33 +0800

    finished basic CoutDownLatch

---
+++ suduo/base/CountDownLatch.h
@@ -0,0 +1,32 @@
+#ifndef COUNTDOWNLATCH_H
+#define COUNTDOWNLATCH_H
+#include "suduo/base/Condition.h"
+#include "suduo/base/Mutex.h"
+namespace suduo {
+class CountDownLatch {
+ public:
+  // TODO count can't smaller than 0
+  explicit CountDownLatch(int count)
+      : _mutex(), _condition(_mutex), _count(count) {}
+
+  void wait() {
+    MutexLockGuard lock(_mutex);
+    while (_count != 0) _condition.wait();
+  }
+
+  void count_down() {
+    MutexLockGuard lock(_mutex);
+    _count--;
+    if (_count == 0) _condition.notifyAll();
+  }
+
+  int get_count() const { return _count; }
+
+ private:
+  mutable MutexLock _mutex;
+  Condition _condition;
+  int _count;
+};
+}  // namespace suduo
+
+#endif
