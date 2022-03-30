9cd2cf789013befea7383ff4c98ebda0544806b5
OliverSu <oliversu15@outlook.com>
2022-03-30 21:03:48 +0800

    finish basic logger

--- suduo/base/Logger.h
+++ suduo/base/Logger.h
@@ -1,14 +1,31 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 #include "suduo/base/LogStream.h"
+#include "suduo/base/Thread.h"
+#include "suduo/base/Timestamp.h"
 namespace suduo {
 class Logger {
  public:
   enum LogLevel { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
 
+  explicit Logger(const char* source, int line);
+  explicit Logger(const char* source, int line, LogLevel level);
+
+  ~Logger();
+
+  void operator<<(LogStream& stream);
+
+  LogStream& stream() { return _stream; }
+
  private:
   LogStream _stream;
+  Timestamp _time;
+  string _source;
+  LogLevel _level;
+  int _line;
 };
 }  // namespace suduo
+
+#define LOG_WARN suduo::Logger(__FILE__, __LINE__, suduo::Logger::WARN).stream()
 
 #endif
---
+++ suduo/base/tests/simple_logger_test.cpp
@@ -0,0 +1,6 @@
+#include "suduo/base/Logger.h"
+
+int main() {
+  LOG_WARN << "OK";
+  return 0;
+}
