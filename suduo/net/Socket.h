c725d50fc7f36575c4acbdbf7896a08a794b054c
OliverSu <oliversu15@outlook.com>
2022-04-07 14:07:51 +0800

    finish basic Socket

--- suduo/net/Socket.h
+++ suduo/net/Socket.h
@@ -1,9 +1,12 @@
 #ifndef SUDUO_SOCKET_H
 #define SUDUO_SOCKET_H
 
+#include <netinet/tcp.h>
+
 #include <utility>
 
 #include "suduo/base/noncopyable.h"
+#include "suduo/net/InetAddress.h"
 namespace suduo {
 namespace net {
 class Socket : noncopyable {
@@ -13,6 +16,20 @@ class Socket : noncopyable {
   ~Socket();
 
   int fd() const { return _sock_fd; }
+
+  bool get_Tcp_info(tcp_info* info) const;
+  bool get_Tcp_info_string(char* buf, int len) const;
+
+  void bind_address(const InetAddress& local_addr);
+  void listen();
+  int accept(InetAddress* peer_addr);
+
+  void shutdown_write();
+
+  void set_Tcp_no_delay(bool on);
+  void set_reuse_addr(bool on);
+  void set_reuse_port(bool on);
+  void set_keep_alive(bool on);
 
  private:
   const int _sock_fd;
