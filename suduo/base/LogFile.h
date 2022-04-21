5ed96800096e5fa99473900414a2df3916c40dba
OliverSu <oliversu15@outlook.com>
2022-04-21 08:01:42 +0800

    change time_t to Timestamp

--- suduo/base/LogFile.h
+++ suduo/base/LogFile.h
@@ -11,9 +11,8 @@ namespace suduo {
 // TODO change later
 class LogFile : noncopyable {
  public:
-  LogFile(const std::string& base_name, int64_t roll_size,
-          bool thread_safe = true, int flush_interval = 3,
-          int check_every_N = 1024);
+  LogFile(const string& base_name, int64_t roll_size, bool thread_safe = true,
+          int flush_interval = 3, int check_every_N = 1024);
   ~LogFile();
 
   void append(const char* log, int len);
@@ -23,23 +22,22 @@ class LogFile : noncopyable {
  private:
   void append_unlocked(const char* log, int len);
 
-  static std::string get_log_filename(const std::string& basename,
-                                      Timestamp now);
+  static string get_log_filename(const string& basename, const Timestamp& now);
 
-  const std::string _basename;
+  const string _basename;
   const off_t _roll_size;
-  const int _flush_interval;
-  const int _check_every_N;
+  const Timestamp _flush_interval;
+  const int _check_every_N_times;
 
   int _count;
 
   std::unique_ptr<MutexLock> _mutex;
-  time_t _start_of_period;
-  time_t _last_roll;
-  time_t _last_flush;
+  Timestamp _start_of_period;
+  Timestamp _last_roll;
+  Timestamp _last_flush;
   std::unique_ptr<FileUtil::FileAppender> _file;
 
-  const static int kRollPerSeconds_ = 60 * 60 * 24;
+  const static int roll_per_seconds_ = 60 * 60 * 24;
 };
 }  // namespace suduo
 #endif
