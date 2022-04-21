7cca1f9ff30ece5a6e367ad809276bea7944ec3b
OliverSu <oliversu15@outlook.com>
2022-04-21 08:19:40 +0800

    refactoring ProcessInfo

--- suduo/base/ProcessInfo.h
+++ suduo/base/ProcessInfo.h
@@ -1,12 +1,25 @@
 #ifndef PROCESSINFO_H
 #define PROCESSINFO_H
 #include <sched.h>
+#include <unistd.h>
 
+#include <charconv>
+#include <cstdint>
 #include <string>
+#include <system_error>
+
+#include "suduo/base/Timestamp.h"
 namespace suduo {
 namespace ProcessInfo {
-pid_t pid();
-std::string pid_string();
+inline pid_t pid() { return getpid(); }
+inline std::string pid_string() {
+  string buf;
+  buf.reserve(32);
+  auto [ptr, ec] = std::to_chars(buf.begin().base(), buf.end().base(),
+                                 static_cast<int>(pid()));
+  if (ec != std::errc()) std::printf("error\n");  // FIXME change the output
+  return buf;
+}
 }  // namespace ProcessInfo
 }  // namespace suduo
 #endif
