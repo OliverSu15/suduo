19d4472983310ae5224fb089dd6942d55a7478ba
OliverSu <oliversu15@outlook.com>
2022-04-25 19:23:07 +0800

    refactoring thread

--- suduo/base/Thread.cpp
+++ suduo/base/Thread.cpp
@@ -14,45 +14,45 @@
 
 #include "suduo/base/CountDownLatch.h"
 #include "suduo/base/CurrentThreadInfo.h"
+#include "suduo/base/Logger.h"
 namespace suduo {
 namespace _detail {
-using Func = std::function<void()>;
-void run_thread_func(Func& _func, pthread_t* _thread_id, pid_t* tid,
+using ThreadFunc = std::function<void()>;
+void run_thread_func(ThreadFunc& _func, pthread_t* _thread_id, pid_t* tid,
                      CountDownLatch* latch, std::string& _name) {
   *tid = Current_thread_info::tid();
   tid = nullptr;
   latch->count_down();
   latch = nullptr;
-  // printf("%u run to here\n", *_thread_id);
   suduo::Current_thread_info::thread_name = _name.c_str();
   pthread_setname_np(*_thread_id, _name.c_str());
 
   try {
     _func();
+  } catch (const Exception& e) {
+    LOG_FATAL << "exception caught in Thread " << _name << "\n"
+              << "reason:" << e.what() << "\n"
+              << "stack trace:" << e.stackTrace() << "\n";
   } catch (const std::exception& e) {
-    // TODO LOG exception info
+    LOG_FATAL << "exception caught in Thread " << _name << "\n"
+              << "reason:" << e.what() << "\n";
   } catch (...) {
-    // TODO LOG exception info
+    LOG_ERROR << "unknown exception caught in Thread" << _name;
     throw;
   }
 }
 void* run(void* it) {
-  Func* ptr = static_cast<Func*>(it);
+  std::shared_ptr<ThreadFunc> ptr(static_cast<ThreadFunc*>(it));
   (*ptr)();
-  delete ptr;
   return nullptr;
 }
 }  // namespace _detail
 }  // namespace suduo
 using Thread = suduo::Thread;
 
-// void suduo::Current_thread_info::cache_thread_id() {
+std::atomic_int32_t Thread::_thread_created;
 
-// }  // TODO 同步名字
-
-std::atomic_int32_t Thread::numCreated_;
-
-Thread::Thread(ThreadFunc func_, const string&& name)
+Thread::Thread(ThreadFunc func_, const string& name)
     : _started(false),
       _joined(false),
       _thread_id(0),
@@ -60,7 +60,7 @@ Thread::Thread(ThreadFunc func_, const string&& name)
       _func(std::move(func_)),
       _name(name),
       _latch(1) {
-  numCreated_++;
+  _thread_created++;
 }
 
 Thread::~Thread() {
@@ -70,19 +70,17 @@ Thread::~Thread() {
 void Thread::start() {
   assert(!_started);
   _started = true;
-  // TODO consider change it to shared_ptr
   auto* pfunc = new ThreadFunc(std::bind(suduo::_detail::run_thread_func, _func,
                                          &_thread_id, &_tid, &_latch, _name));
-  int err = 0;
-  if (err = pthread_create(&_thread_id, nullptr, &suduo::_detail::run, pfunc)) {
+  int err = pthread_create(&_thread_id, nullptr, &suduo::_detail::run, pfunc);
+  if (err) {
     _started = false;
     delete pfunc;
-    // TODO log error
+    LOG_SYSFATAL << "Failed in pthread_create";
   } else {
     _latch.wait();
-    // assert(tid_ > 0);
-    // printf("thread_create success\n");
-    // TODO assert it do create a thread
+    assert(_tid > 0);
+    LOG_DEBUG << "Thread create success";
   }
 }
 
@@ -90,23 +88,5 @@ void Thread::join() {
   assert(_started);
   assert(!_joined);
   _joined = true;
-  int err = 0;
-  if ((err = pthread_join(_thread_id, nullptr))) {
-    switch (err) {
-      case EDEADLK:
-        printf("A deadlock was detected\n");
-        break;
-      case EINVAL:
-        printf("thread is not a joinable thread\n");
-        break;
-      case ESRCH:
-        printf("No thread with the ID thread could be found.\n");
-        break;
-      default:
-        printf("unknow error\n");
-        break;
-    }
-    // TODO handle error
-  }
-  // printf("join success");
+  ERROR_CHECK(pthread_join(_thread_id, nullptr));
 }
--- suduo/base/Thread.h
+++ suduo/base/Thread.h
@@ -10,15 +10,15 @@
 #include "suduo/base/CountDownLatch.h"
 #include "suduo/base/noncopyable.h"
 namespace suduo {
-using string = std::string;
-namespace _detail {
-void* run(void* it);
-}
+// using string = std::string;
+// namespace _detail {
+// void* run(void* it);
+// }
 
 class Thread : noncopyable {
  public:
   using ThreadFunc = std::function<void()>;
-  explicit Thread(ThreadFunc func_, const string&& name = "suduo");
+  explicit Thread(ThreadFunc func, const string& name = "suduo");
   ~Thread();
 
   void start();
@@ -28,10 +28,10 @@ class Thread : noncopyable {
   bool started() const { return _started; }
   pid_t tid() const { return _tid; }
   const string& name() const { return _name; }
-  static int numCreated() { return numCreated_.load(); }
+  static int numCreated() { return _thread_created; }
 
  private:
-  friend void* suduo::_detail::run(void* it);
+  // friend void* suduo::_detail::run(void* it);
   bool _started = false;
   bool _joined = false;
   pthread_t _thread_id;
@@ -41,7 +41,7 @@ class Thread : noncopyable {
   CountDownLatch _latch;
   // exits only to use the muduo test more easily
   // TODO remove
-  static std::atomic_int32_t numCreated_;
+  static std::atomic_int32_t _thread_created;
 };
 }  // namespace suduo
 #endif
